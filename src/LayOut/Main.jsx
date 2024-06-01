caa4b4c2938cbfc27fc93569478d6b88b9c9aa65
Ancishan <shantodev027@gmail.com>
2024-06-01 20:22:25 +0600

    [DONE] Lognin page

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Component/Shared/Navbar";
 
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
            <Outlet></Outlet>
         </div>
     );
